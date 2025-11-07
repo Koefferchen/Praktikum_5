0cec7bd9f17239e251a268676a3850438a55828c
Koefferchen <hostedbysome@gmail.com>
2025-11-07 23:35:35 +0100

    vollständige Auswertung + unvollständige Aufbau_Skizzen

--- V525/Code/Energie-Kalibration.py
+++ V525/Code/Energie-Kalibration.py
@@ -1,5 +1,5 @@
 
-from MyPyLib import *
+from MyPyLib_v2 import *
 
 
 # -------------------------- Plotting of raw energy spectrums --------------------------
@@ -19,7 +19,7 @@ def plot_raw_data( n ):
     channels    = data[ : , 0]
     counts      = data[ : , 1]
     if ((n == 2) or (n == 5)):
-        color = sns.color_palette("bright")[2]
+        color = sns.color_palette("bright")[0]
     else:
         color = sns.color_palette("bright")[0]
 
@@ -45,15 +45,16 @@ def plot_raw_data( n ):
     zoom_params         = no_zooming.copy()
     colorbar_params     = no_colorbar.copy()
     extra_label         = no_extra_label.copy()
-    
+    extra_xaxis         = no_extra_xaxis.copy()
+
     data_set_1  = channels, None, counts, None
     
     all_data                = [ data_set_1 ]                               
     all_sample_format_dicts = [ sample_format_dict_1 ]
 
     save_plot = True, "../Figures/"+dateinamen[n]+".jpg"                                     
-    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, save_plot, all_sample_format_dicts, general_format_dict)
-
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
+plot_raw_data(1)
 plot_raw_data(1)
 #for j in range(6):
 #    plot_raw_data(j)
@@ -62,7 +63,7 @@ plot_raw_data(1)
 # -------------------------- Gauss-Plotting Na  --------------------------
 
 
-def plot_fit_data_Na(exp_data, fit_data, chi, parity, id ):
+def plot_fit_data_Na(exp_data, fit_data, chi, parity, id, E_of_C=None, C_of_E=None ):
 
     sample_format_dict_1 = {
         "label"      : "Messdaten",          
@@ -94,23 +95,32 @@ def plot_fit_data_Na(exp_data, fit_data, chi, parity, id ):
     colorbar_params     = no_colorbar.copy()
     extra_label         = no_extra_label.copy()
     
-
+    if ((E_of_C == None) or (C_of_E == None)):
+        extra_xaxis = no_extra_xaxis.copy()
+    else:
+        extra_xaxis         = {
+            "do_axis"   :   True,
+            "f_new(old)":   E_of_C,
+            "f_old(new)":   C_of_E,
+            "label"     :   r"Energie $E$ [keV]"
+        }
     
     all_data                = [ exp_data, fit_data ]                               
     all_sample_format_dicts = [ sample_format_dict_1, sample_format_dict_gauss ]
 
     save_plot = True, "../Figures/0"+id+"_Na_"+parity+"_511keV_Fit.jpg"                                     
-    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, save_plot, all_sample_format_dicts, general_format_dict)
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
 
 
 # -------------------------- Gauss-Fit Na (Left) --------------------------
 
 
 data_left = np.loadtxt("../Data/"+dateinamen[1]+".txt")
-channels_left    = data_left[ : , 0]
-counts_left      = data_left[ : , 1]
-counts_err_left  = np.sqrt(counts_left)+1
-exp_data_Na_left = [channels_left, None, counts_left, counts_err_left ]
+Na_channels_left    = data_left[ : , 0]
+Na_counts_left      = data_left[ : , 1]
+Na_counts_err_left  = np.sqrt(Na_counts_left)+1
+exp_data_Na_left    = [Na_channels_left, None, Na_counts_left, Na_counts_err_left ]
+exp_noerr_Na_left   = [Na_channels_left, None, Na_counts_left, None ]
 
 fitbook_Na_left = {
     "data_set"          : exp_data_Na_left,
@@ -122,17 +132,18 @@ fitbook_Na_left = {
 }
 fit_data_Na_left, params_Na_left_511, params_err_Na_left_511, chi_Na_left = ultimate_fit( fitbook_Na_left )
 
-plot_fit_data_Na( [channels_left, None, counts_left, None], fit_data_Na_left, chi_Na_left, "Links", "2" )
+plot_fit_data_Na( exp_noerr_Na_left, fit_data_Na_left, chi_Na_left, "Links", "2" )
 
 
 # -------------------------- Gauss-Fit Na (Right) --------------------------
 
 
 data_right = np.loadtxt("../Data/"+dateinamen[4]+".txt")
-channels_right    = data_right[ : , 0]
-counts_right      = data_right[ : , 1]
-counts_err_right  = np.sqrt(counts_right)+1
-exp_data_Na_right  = [channels_right, None, counts_right, counts_err_right ]
+Na_channels_right    = data_right[ : , 0]
+Na_counts_right      = data_right[ : , 1]
+Na_counts_err_right  = np.sqrt(Na_counts_right)+1
+exp_data_Na_right    = [Na_channels_right, None, Na_counts_right, Na_counts_err_right ]
+exp_noerr_Na_right    = [Na_channels_right, None, Na_counts_right, None ]
 
 fitbook_Na_right = {
     "data_set"          : exp_data_Na_right,
@@ -144,14 +155,14 @@ fitbook_Na_right = {
 }
 fit_data_Na_right, params_Na_right_511, params_err_Na_right_511, chi_Na_right = ultimate_fit( fitbook_Na_right )
 
-plot_fit_data_Na( [channels_right, None, counts_right, None], fit_data_Na_right, chi_Na_right, "Rechts", "5" )
+plot_fit_data_Na( exp_noerr_Na_right, fit_data_Na_right, chi_Na_right, "Rechts", "5" )
 
 
 
 # -------------------------- Gauss-Plotting Ba --------------------------
 
 
-def plot_fit_data_Ba( exp_data, fit_datas, chis, parity, id ):
+def plot_fit_data_Ba( exp_data, fit_datas, chis, parity, id, E_of_C=None, C_of_E=None ):
 
     fit_colors  = sns.color_palette("magma", 6)
     fit_lables  = [ f"Fit an 31keV-Linie $(\\chi^2 = {chis[0]:.1f})$",
@@ -189,6 +200,16 @@ def plot_fit_data_Ba( exp_data, fit_datas, chis, parity, id ):
     zoom_params         = no_zooming.copy()
     colorbar_params     = no_colorbar.copy()
     extra_label         = no_extra_label.copy()
+    
+    if ((E_of_C == None) or (C_of_E == None)):
+        extra_xaxis = no_extra_xaxis.copy()
+    else:
+        extra_xaxis         = {
+            "do_axis"   :   True,
+            "f_new(old)":   E_of_C,
+            "f_old(new)":   C_of_E,
+            "label"     :   r"Energie $E$ [keV]"
+        }
 
     
     all_data                = [ exp_data, *fit_datas ]                               
@@ -200,18 +221,19 @@ def plot_fit_data_Ba( exp_data, fit_datas, chis, parity, id ):
         all_sample_format_dicts.append( sample_format_dict_gauss.copy() )
 
     save_plot = True, "../Figures/0"+id+"_Ba_"+parity+"_Fits.jpg"                                     
-    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, save_plot, all_sample_format_dicts, general_format_dict)
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
 
 
 # -------------------------- Gauss-Fits Ba (Left) --------------------------
 
 
 data = np.loadtxt("../Data/"+dateinamen[2]+".txt")
-channels_left    = data[ : , 0]
-counts_left      = data[ : , 1]
-counts_err_left  = np.sqrt(counts_left)+1
+Ba_channels_left    = data[ : , 0]
+Ba_counts_left      = data[ : , 1]
+Ba_counts_err_left  = np.sqrt(Ba_counts_left)+1
 
-exp_data_Ba_left = [channels_left, None, counts_left, counts_err_left ]
+exp_data_Ba_left = [Ba_channels_left, None, Ba_counts_left, Ba_counts_err_left ]
+exp_noerr_Ba_left = [Ba_channels_left, None, Ba_counts_left, None ]
 
 fitbook_Ba_left_31keV = {
     "data_set"          : exp_data_Ba_left,
@@ -254,7 +276,7 @@ fit_data_Ba_left_356keV, params_Ba_left_356keV, params_err_Ba_left_356keV, chi_B
 fit_datas_Ba_left   = [fit_data_Ba_left_31keV, fit_data_Ba_left_81keV, fit_data_Ba_left_302keV, fit_data_Ba_left_356keV]
 chis_Ba_left        = [chi_Ba_left_31keV, chi_Ba_left_81keV, chi_Ba_left_302keV, chi_Ba_left_356keV]
 
-plot_fit_data_Ba( [channels_left, None, counts_left, None ], fit_datas_Ba_left, chis_Ba_left, "Links", "3")
+plot_fit_data_Ba( exp_noerr_Ba_left, fit_datas_Ba_left, chis_Ba_left, "Links", "3")
 
 Kalibration_params_left = np.array([ params_Ba_left_31keV, params_Ba_left_81keV, params_Ba_left_302keV, params_Ba_left_356keV, params_Na_left_511 ])
 Kalibration_params_left_err = np.array([ params_err_Ba_left_31keV, params_err_Ba_left_81keV, params_err_Ba_left_302keV, params_err_Ba_left_356keV, params_err_Na_left_511])
@@ -266,11 +288,12 @@ array_to_latex( "../Data/Params_Kalibration_Links.txt", Kalibration_params_left.
 
 
 data = np.loadtxt("../Data/"+dateinamen[5]+".txt")
-channels_right    = data[ : , 0]
-counts_right      = data[ : , 1]
-counts_err_right  = np.sqrt(counts_right)+1
+Ba_channels_right   = data[ : , 0]
+Ba_counts_right     = data[ : , 1]
+Ba_counts_err_right = np.sqrt(Ba_counts_right)+1
 
-exp_data_Ba_right = [channels_right, None, counts_right, counts_err_right ]
+exp_data_Ba_right   = [Ba_channels_right, None, Ba_counts_right, Ba_counts_err_right ]
+exp_noerr_Ba_right  = [Ba_channels_right, None, Ba_counts_right, None ]
 
 fitbook_Ba_right_31keV = {
     "data_set"          : exp_data_Ba_right,
@@ -313,10 +336,10 @@ fit_data_Ba_right_356keV, params_Ba_right_356keV, params_err_Ba_right_356keV, ch
 fit_datas_Ba_right   = [fit_data_Ba_right_31keV, fit_data_Ba_right_81keV, fit_data_Ba_right_302keV, fit_data_Ba_right_356keV]
 chis_Ba_right        = [chi_Ba_right_31keV, chi_Ba_right_81keV, chi_Ba_right_302keV, chi_Ba_right_356keV]
 
-plot_fit_data_Ba( [channels_right, None, counts_right, None ], fit_datas_Ba_right, chis_Ba_right, "Rechts", "6")
+plot_fit_data_Ba( exp_noerr_Ba_right, fit_datas_Ba_right, chis_Ba_right, "Rechts", "6")
 
-Kalibration_params_right = np.array([ params_Ba_right_31keV, params_Ba_right_81keV, params_Ba_right_302keV, params_Ba_right_356keV, params_Na_right_511 ])
-Kalibration_params_right_err = np.array([ params_err_Ba_right_31keV, params_err_Ba_right_81keV, params_err_Ba_right_302keV, params_err_Ba_right_356keV, params_err_Na_right_511])
+Kalibration_params_right        = np.array([ params_Ba_right_31keV, params_Ba_right_81keV, params_Ba_right_302keV, params_Ba_right_356keV, params_Na_right_511 ])
+Kalibration_params_right_err    = np.array([ params_err_Ba_right_31keV, params_err_Ba_right_81keV, params_err_Ba_right_302keV, params_err_Ba_right_356keV, params_err_Na_right_511])
 
 array_to_latex( "../Data/Params_Kalibration_Rechts.txt", Kalibration_params_right.T, Kalibration_params_right_err.T)
 
@@ -356,13 +379,14 @@ def plot_energie_kalib(exp_data, fit_data, chi, parity):
     zoom_params         = no_zooming.copy()
     colorbar_params     = no_colorbar.copy()
     extra_label         = no_extra_label.copy()
-    
+    extra_xaxis         = no_extra_xaxis.copy()
+ 
     
     all_data                = [ exp_data, fit_data ]                               
     all_sample_format_dicts = [ sample_format_dict_1, sample_format_dict_2 ]
 
     save_plot = True, "../Figures/Energy_Kalibration_Fit_"+parity+".jpg"                                     
-    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, save_plot, all_sample_format_dicts, general_format_dict)
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
 
 def energy_of_channel( channel, channel_err, params_kalib, params_kalib_err ):
     a           = params_kalib[0]
@@ -431,4 +455,88 @@ print("E_err rechts: ", E_peaks_err_right)
 
 array_to_latex("../Data/Params_E_kalib.txt", np.array([params_kalib_left, params_kalib_right]).T, np.array([params_err_kalib_left, params_err_kalib_right]).T, ".4f" )
 array_to_latex("../Data/Results_E.txt", np.array([energy_array, E_peaks_left, E_peaks_right ]), np.array([energy_err_array, E_peaks_err_left, E_peaks_err_right]), ".4f")
+
+
+
+# -------------------------- Plotting mit Energie-Achse --------------------------
+
+
+a_L          = params_kalib_left[0]
+b_L          = params_kalib_left[1]
+a_R          = params_kalib_right[0]
+b_R          = params_kalib_right[1]
+
+def C_L(E_L):
+    return a_L*E_L + b_L 
+def C_R(E_R):
+    return a_R*E_R + b_R
+def E_L(C_L):
+    return (C_L - b_L)/a_L 
+def E_R(C_R):
+    return (C_R - b_R)/a_R 
+
+
+def plot_raw_kalibrated( dateiname, E_of_C, C_of_E ):
+    
+    data = np.loadtxt("../Data/"+dateiname+".txt")
+    channels    = data[ : , 0]
+    counts      = data[ : , 1]
+
+    color = sns.color_palette("bright")[0]
+
+
+    sample_format_dict_1 = {
+        "label"      : None,          
+        "fmt"        : 'o', 
+        "color"      : color,                               
+        "markersize" : 0.7, 
+        "linewidth"  : 1,
+        "capsize"    : 0,
+        "alpha"      : 0.6                                   
+    }
+
+    writtings = {
+        "title"       : None,
+        "x_ax_label"  : r"Kanal-Index $K$ [1]",
+        "y_ax_label"  : r"Ereignisse $N$ [1]"
+    }
+    
+    general_format_dict = standard_format_dict.copy()
+    zoom_params         = no_zooming.copy()
+    colorbar_params     = no_colorbar.copy()
+    extra_label         = no_extra_label.copy()
+    extra_xaxis         = {
+        "do_axis"   :   True,
+        "f_new(old)":   E_of_C,
+        "f_old(new)":   C_of_E,
+        "label"     :   r"Energie $E$ [keV]"
+    }
+
+    data_set_1  = channels, None, counts, None
+    
+    all_data                = [ data_set_1 ]                               
+    all_sample_format_dicts = [ sample_format_dict_1 ]
+
+    save_plot = True, "../Figures/"+dateiname+".jpg"                                     
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
+
+
+plot_raw_kalibrated( "07_Na_Spektrum_Fenster_Links", E_L, C_L )
+plot_raw_kalibrated( "08_Na_Spektrum_Fenster_Rechts", E_R, C_R )
+
+
+    # überschreibe frühere Gaußfitplots, da nun Energieskalierung bekannt:
+plot_fit_data_Na( exp_noerr_Na_left, fit_data_Na_left, chi_Na_left, "Links", "2", E_L, C_L )
+plot_fit_data_Na( exp_noerr_Na_right, fit_data_Na_right, chi_Na_right, "Rechts", "5", E_R, C_R )
+plot_fit_data_Ba( exp_noerr_Ba_left, fit_datas_Ba_left, chis_Ba_left, "Links", "3", E_L, C_L)
+plot_fit_data_Ba( exp_noerr_Ba_right, fit_datas_Ba_right, chis_Ba_right, "Rechts", "6", E_R, C_R)
+
+
+# -------------------------- Ba mit mit CFD-Schwelle auf 0 --------------------------
+
+plot_raw_kalibrated( "09_Ba_Spektrum_CFD_Links", E_L, C_L )
+plot_raw_kalibrated( "10_Ba_Spektrum_CFD_Rechts", E_R, C_R )
+plot_raw_kalibrated( "12_Ba_Links_356_Fenster", E_L, C_L )
+plot_raw_kalibrated( "13_Ba_Rechts_81_Fenster", E_R, C_R )
+
 
---
+++ V525/Code/Lebensdauer-Kurve.py
@@ -0,0 +1,342 @@
+
+from MyPyLib_v2 import *
+from scipy.special import erf
+
+# ------------------------ Promptkurve plotten ------------------------
+
+def plot_promptkurven( exp_data, fit_datas=None, chis=None, T_of_C=None, C_of_T=None ):
+
+    sample_format_dict_1 = {
+        "label"      : "Messdaten",          
+        "fmt"        : 'o', 
+        "color"      : sns.color_palette("bright")[9],                               
+        "markersize" : 1.0, 
+        "linewidth"  : 1,
+        "capsize"    : 0,
+        "alpha"      : 0.6                                   
+    }
+
+    sample_format_dict_gauss = {
+        "label"      : None,          
+        "fmt"        : '-', 
+        "color"      : None,                               
+        "markersize" : 0.7, 
+        "linewidth"  : 2,
+        "capsize"    : 0,
+        "alpha"      : 1                                   
+    }
+
+    writtings = {
+        "title"       : None,
+        "x_ax_label"  : r"Kanal-Index $K$ [1]",
+        "y_ax_label"  : r"Ereignisse $N$ [1]"
+    }
+    
+    general_format_dict = standard_format_dict.copy()
+    zoom_params         = no_zooming.copy()
+    colorbar_params     = no_colorbar.copy()
+    extra_label         = no_extra_label.copy()
+    
+    if ((T_of_C == None) or (C_of_T == None)):
+        extra_xaxis = no_extra_xaxis.copy()
+    else:
+        extra_xaxis         = {
+            "do_axis"   :   True,
+            "f_new(old)":   T_of_C,
+            "f_old(new)":   C_of_T,
+            "label"     :   r"Zeit $t$ [ns]"
+        }
+
+
+    all_sample_format_dicts = [ sample_format_dict_1 ]
+
+    if(fit_datas == None):
+        save_plot = True, "../Figures/Na_Promptkurven.jpg"
+        all_data = [exp_data]
+    else:
+        save_plot = True, "../Figures/Na_Promptkurven_T.jpg"
+        all_data = [ exp_data, *fit_datas ] 
+        fit_colors  = sns.color_palette("magma", len(fit_datas))                           
+        for i in range(len(fit_datas)):
+            sample_format_dict_gauss["label"] = f"$\\chi^2 = {chis[i]:.1f}$"
+            sample_format_dict_gauss["color"] = fit_colors[i]
+            all_sample_format_dicts.append( sample_format_dict_gauss.copy() )
+
+                     
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
+
+
+
+pro_data_txt    = np.loadtxt("../Data/11_Na_Promptkurve_16ns.txt")
+pro_channel     = pro_data_txt[ : , 0]
+pro_count       = pro_data_txt[ : , 1]
+pro_count_err   = np.sqrt(pro_count)+1
+pro_data        = [pro_channel, None, pro_count, pro_count_err]
+pro_data_noerr  = [pro_channel, None, pro_count, None]
+
+    # Plot der Rohdaten
+plot_promptkurven(pro_data_noerr)
+
+
+# ------------------------ Promptkurve fitten ------------------------
+
+
+
+density = 400
+
+fitbook_1 = {
+    "data_set"          : pro_data,
+    "region_of_interest": [500, 1200],
+    "fit_function"      : gauss_fit,
+    "params_guess"      : [800, 200, 40],
+    "region_of_fit"     : None,
+    "fit_density"       : density
+}
+fitbook_2 = {
+    "data_set"          : pro_data,
+    "region_of_interest": [1800, 2400],
+    "fit_function"      : gauss_fit,
+    "params_guess"      : [2100, 200, 40],
+    "region_of_fit"     : None,
+    "fit_density"       : density
+}
+fitbook_3 = {
+    "data_set"          : pro_data,
+    "region_of_interest": [3050, 3650],
+    "fit_function"      : gauss_fit,
+    "params_guess"      : [3300, 200, 40],
+    "region_of_fit"     : None,
+    "fit_density"       : density
+}
+fitbook_4 = {
+    "data_set"          : pro_data,
+    "region_of_interest": [4200, 5000],
+    "fit_function"      : gauss_fit,
+    "params_guess"      : [4700, 200, 40],
+    "region_of_fit"     : None,
+    "fit_density"       : density
+}
+fitbook_5 = {
+    "data_set"          : pro_data,
+    "region_of_interest": [5500, 6200],
+    "fit_function"      : gauss_fit,
+    "params_guess"      : [5800, 200, 40],
+    "region_of_fit"     : None,
+    "fit_density"       : density
+}
+fitbook_6 = {
+    "data_set"          : pro_data,
+    "region_of_interest": [6900, 7500],
+    "fit_function"      : gauss_fit,
+    "params_guess"      : [7200, 200, 40],
+    "region_of_fit"     : None,
+    "fit_density"       : density
+}
+
+fitbooks        = [fitbook_1, fitbook_2, fitbook_3, fitbook_4, fitbook_5, fitbook_6]
+pro_fit_data_all= []
+pro_params      = np.zeros((len(fitbooks), 3))
+pro_params_err  = np.zeros((len(fitbooks), 3))
+pro_chis        = np.zeros(len(fitbooks))
+
+for i, book in enumerate(fitbooks):
+    pro_fit_data_i, pro_params[i], pro_params_err[i], pro_chis[i] = ultimate_fit(book)
+    pro_fit_data_all.append(pro_fit_data_i)
+
+
+    # überarbeiteter Plot, jetzt mit Fit
+plot_promptkurven(pro_data_noerr, pro_fit_data_all, pro_chis )
+
+
+array_to_latex("../Data/Params_Promtkurve_Fits.txt", pro_params.T, pro_params_err.T, ".4f")
+
+
+# ------------------------ Zeit_Achse fitten ------------------------
+
+
+pro_peaks       = pro_params[ : , 0]
+pro_peaks_err   = pro_params_err[ : , 0]
+time_array      = np.array([0, 16, 32, 48, 64, 80])
+peak_data       = [time_array, None, pro_peaks, pro_peaks_err]
+
+fitbook_time = {
+    "data_set"          : peak_data,
+    "region_of_interest": None,
+    "fit_function"      : linear_fit,
+    "params_guess"      : None,
+    "region_of_fit"     : None,
+    "fit_density"       : 2
+}
+time_fit, time_params, time_params_err, time_chi = ultimate_fit( fitbook_time )
+
+
+def plot_energie_kalib(exp_data, fit_data, chi):
+    
+    sample_format_dict_1 = {
+        "label"      : r"Messdaten",          
+        "fmt"        : 'o', 
+        "color"      : sns.color_palette("dark")[0],                               
+        "markersize" : 4, 
+        "linewidth"  : 1,
+        "capsize"    : 0,
+        "alpha"      : 1                                   
+    }
+    sample_format_dict_2 = {
+        "label"      : f"Linearer Fit $(\\chi^2 = {chi:.1f})$",                       
+        "fmt"        : '--', 
+        "color"      : sns.color_palette("dark")[9],        
+        "markersize" : 4, 
+        "linewidth"  : 1,
+        "capsize"    : 0,
+        "alpha"      : 1
+    }  
+
+    writtings = {
+        "title"       : None,
+        "y_ax_label"  : r"Kanal-Index $K$ [1]",
+        "x_ax_label"  : r"Zeit $t$ [ns]"
+    }
+    
+    general_format_dict = standard_format_dict.copy()
+    zoom_params         = no_zooming.copy()
+    colorbar_params     = no_colorbar.copy()
+    extra_label         = no_extra_label.copy()
+    extra_xaxis         = no_extra_xaxis.copy()
+ 
+    
+    all_data                = [ exp_data, fit_data ]                               
+    all_sample_format_dicts = [ sample_format_dict_1, sample_format_dict_2 ]
+
+    save_plot = True, "../Figures/Zeit-Kalibration.jpg"                                     
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
+plot_energie_kalib(peak_data, time_fit, time_chi)
+
+
+a       = time_params[0]
+b       = time_params[1]
+a_err   = time_params_err[0]
+b_err   = time_params_err[1]
+
+print("Zeitkalibration: Channel = a*Zeit + b")
+print(f"a = ({a:.2f}+- {a_err:.2f})/ns")
+print(f"b = ({b:.2f}+- {b_err:.2f})")
+
+def C_of_T(time):
+    return a*time + b
+def T_of_C(channel):
+    return (channel - b)/a
+
+
+    # überarbeiteter Plot, jetzt mit Zeitachse
+plot_promptkurven(pro_data_noerr, pro_fit_data_all, pro_chis, T_of_C, C_of_T )
+
+
+
+# ------------------------ Lebenskurve plotten ------------------------
+
+
+decay_data_txt  = np.loadtxt("../Data/14_Ba_Lebenskurve.txt")
+decay_channel   = decay_data_txt[:, 0]
+decay_counts    = decay_data_txt[:, 1]
+decay_counts_err= np.sqrt(decay_counts)+1
+decay_data_time = [T_of_C(decay_channel), None, decay_counts, decay_counts_err]
+decay_data_noerr= [decay_channel, None, decay_counts, None]
+
+
+def plot_lebenskurve( exp_data, fit_data=None, chi=None, T_of_C=None, C_of_T=None ):
+
+    if(chi == None):
+        chi = 0
+
+    sample_format_dict_1 = {
+        "label"      : "Messdaten",          
+        "fmt"        : 'o', 
+        "color"      : sns.color_palette("bright")[9],                               
+        "markersize" : 1.0, 
+        "linewidth"  : 1,
+        "capsize"    : 0,
+        "alpha"      : 0.6                                   
+    }
+
+    sample_format_dict_fit = {
+        "label"      : f"Fit ($\\chi^2 = {chi:.1f}$)",          
+        "fmt"        : '-', 
+        "color"      : sns.color_palette("magma", 2)[0],                               
+        "markersize" : 0.7, 
+        "linewidth"  : 2,
+        "capsize"    : 0,
+        "alpha"      : 1                                   
+    }
+
+    writtings = {
+        "title"       : None,
+        "x_ax_label"  : r"Kanal-Index $K$ [1]",
+        "y_ax_label"  : r"Ereignisse $N$ [1]"
+    }
+    
+    general_format_dict = standard_format_dict.copy()
+    zoom_params         = no_zooming.copy()
+    colorbar_params     = no_colorbar.copy()
+    extra_label         = no_extra_label.copy()
+    
+    if ((T_of_C == None) or (C_of_T == None)):
+        extra_xaxis = no_extra_xaxis.copy()
+    else:
+        extra_xaxis         = {
+            "do_axis"   :   True,
+            "f_new(old)":   T_of_C,
+            "f_old(new)":   C_of_T,
+            "label"     :   r"Zeit $t$ [ns]"
+        }
+
+    if(fit_data == None):
+        save_plot               = True, "../Figures/Ba_Lebenskurve.jpg"
+        all_data                = [exp_data]
+        all_sample_format_dicts = [ sample_format_dict_1 ]
+
+    else:
+        save_plot               = True, "../Figures/Ba_Lebenskurve_T.jpg"
+        all_data                = [ exp_data, fit_data ] 
+        all_sample_format_dicts = [ sample_format_dict_1, sample_format_dict_fit ]
+                     
+    ultimate_plot_advanced (all_data, writtings, zoom_params, colorbar_params, extra_label, extra_xaxis, save_plot, all_sample_format_dicts, general_format_dict)
+
+
+    # Plotten der Rohdaten mit Zeit-Achse 
+plot_lebenskurve(decay_data_noerr, None, None, T_of_C, C_of_T)
+
+
+# ------------------------ Lebenskurve fitten ------------------------
+
+
+    # Fitfunktion nach Anleitung. Eigentlich müsst vor erf() noch "np.sqrt(np.pi)/2" stehen.
+def faltung_fit( t, tau, t_0, sigma, I):
+    return I/(2*tau) * np.exp( (sigma**2 - 2*tau*(t-t_0))/(2*tau**2) ) * ( 1 + erf( 2**(-1/2) *((t-t_0)/sigma - sigma/tau ) ) )
+
+fitbook_leben = {
+    "data_set"          : decay_data_time,
+    "region_of_interest": None,
+    "fit_function"      : faltung_fit,
+    "params_guess"      : [9, 8, 5, 100/(2*9)],
+    "region_of_fit"     : None,
+    "fit_density"       : 400
+}
+
+decay_fit, decay_params, decay_params_err, decay_chi = ultimate_fit( fitbook_leben )
+decay_fit[0] = C_of_T(decay_fit[0])
+
+plot_lebenskurve(decay_data_noerr, decay_fit, decay_chi, T_of_C, C_of_T)
+
+
+tau, t_0, sigma, I                  = decay_params
+tau_err, t_0_err, sigma_err, I_err  = decay_params_err
+t_halb, t_halb_err                  = tau * np.log(2), tau_err * np.log(2)
+
+print("Lebenskurve Fitparameter:")
+print(f"tau   = ({tau:.2f} +- {tau_err:.2f})ns ")
+print(f"t_halb= ({t_halb:.2f} +- {t_halb_err:.2f})ns ")
+print(f"t_0   = ({t_0:.2f} +- {t_0_err:.2f})ns")
+print(f"sigma = ({sigma:.2f} +- {sigma_err:.2f})ns")
+print(f"I     = ({I:.2f} +- {I_err:.2f})")
+
+
